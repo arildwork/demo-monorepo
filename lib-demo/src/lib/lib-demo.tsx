d216e7c946f3570f9452ba75f1d625218e5509aa
Arild Sela <arild.sela@sequello.com>
2025-09-30 15:13:49 +0200

    First commit

---
+++ lib-demo/src/lib/lib-demo.tsx
@@ -0,0 +1,11 @@
+import styles from './lib-demo.module.scss';
+
+export function DemoLibDemo() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to DemoLibDemo!</h1>
+    </div>
+  );
+}
+
+export default DemoLibDemo;
